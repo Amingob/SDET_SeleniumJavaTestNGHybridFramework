ba630a0af2a0f7de5382acc15999dcf40e1d7b28
Amingo <SesiNyeleti79!>
2024-05-03 03:50:00 +0200

    commit pom.xmlfile

--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -1,5 +1,25 @@
 package basetest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class BaseTest {
+
+    public WebDriver wdriver;
+    @BeforeClass
+    public void setUp(){
+
+        System.setProperty("webdriver.edge.driver","./Drivers/msedgedriver.exe");
+        wdriver = new EdgeDriver();
+        wdriver.get("https://demo.guru99.com/V4/");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        wdriver.quit();
+    }
 
 }
